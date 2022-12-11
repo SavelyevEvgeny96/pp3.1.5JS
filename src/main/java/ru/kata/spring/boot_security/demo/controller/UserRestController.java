55d3d0d47e5d72e5b84f595380705f669616f4e2
rodnoy96 <Zhenyasavelyev08494@gmail.com>
2022-12-11 15:32:54 +0300

    без нью респонс

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserRestController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserRestController.java
@@ -2,13 +2,12 @@ package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.service.UserService;
-
-import java.security.Principal;
 
 @RestController
 @RequestMapping("api/")
@@ -21,7 +20,7 @@ public class UserRestController {
     }
 
     @GetMapping("/user")
-    public ResponseEntity<User> getInfoCurrentUser(Principal principal) {
-        return new ResponseEntity<>(userService.getUserByName(principal.getName()), HttpStatus.OK); // 200
+    public ResponseEntity<User> getInfoCurrentUser(@AuthenticationPrincipal User user) {
+        return  ResponseEntity.ok(userService.getUserByName(user.getUsername()));
     }
 }
